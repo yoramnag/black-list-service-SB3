3face9835e8bdbfb28b7493d457039aea88418ab
yoram <yoramnag@gmail.com>
2024-06-03 18:48:04 +0300

    adding black list Entity and data.sql to the project

--- src/main/java/com/example/blackListService/entity/BlackList.java
+++ src/main/java/com/example/blackListService/entity/BlackList.java
@@ -1,4 +1,24 @@
 package com.example.blackListService.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class BlackList {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="credit_card")
+    private String creditCard;
+
+    @Column(name="mask_credit_card")
+    private String maskCreditCard;
 }
