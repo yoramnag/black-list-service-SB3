3cb8c58c181e333d81b4558b925e769d89210f04
yoram <yoramnag@gmail.com>
2024-06-15 18:13:59 +0300

    add validation and move to RequestParam

--- src/main/java/com/example/blackListService/dto/BlackListDto.java
+++ src/main/java/com/example/blackListService/dto/BlackListDto.java
@@ -1,11 +1,16 @@
 package com.example.blackListService.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class BlackListDto {
 
+    @NotEmpty(message = "credit card can NOT be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{16})", message = "credit card must be 16 digits")
     private String creditCard;
+
     private String maskCreditCard;
 
 }
--- src/main/java/com/example/blackListService/rest/BlackListRestController.java
+++ src/main/java/com/example/blackListService/rest/BlackListRestController.java
@@ -4,10 +4,13 @@ import com.example.blackListService.constants.BlackListConstants;
 import com.example.blackListService.dto.BlackListDto;
 import com.example.blackListService.dto.ResponseDto;
 import com.example.blackListService.service.BlackListService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/api" , produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@Validated
 public class BlackListRestController {
 
     private BlackListService blackListService;
@@ -24,7 +28,7 @@ public class BlackListRestController {
      * get all records from BlackList table
      * @return all black list cards from the BlackList table
      */
-    @GetMapping("/blacklist")
+    @GetMapping("/getAllBlacklist")
     public ResponseEntity<List<BlackListDto>> retrieveBlackListCard() {
         List<BlackListDto> blackListDtos = blackListService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(blackListDtos);
@@ -35,8 +39,10 @@ public class BlackListRestController {
      * @param creditCardNumber black list credit card number
      * @return status OK if credit card exist on the BlackList table
      */
-    @GetMapping("/blacklist/{creditCardNumber}")
-    public ResponseEntity<BlackListDto> retrieveBlackListCard(@PathVariable String creditCardNumber) {
+    @GetMapping("/getBlacklist")
+    public ResponseEntity<BlackListDto> retrieveBlackListCard(@RequestParam
+                                                                  @Pattern(regexp = "(^$|[0-9]{16})", message = "credit card must be 16 digits")
+                                                                  String creditCardNumber) {
         BlackListDto blackListDto = blackListService.findByCreditCardNumber(creditCardNumber);
         return ResponseEntity.status(HttpStatus.OK).body(blackListDto);
     }
@@ -46,8 +52,8 @@ public class BlackListRestController {
      * @param blackListDto credit card info
      * @return status created if credit card successfully added to the BlackList table
      */
-    @PostMapping("/blacklist")
-    public ResponseEntity<ResponseDto> createBlackListCard(@RequestBody BlackListDto blackListDto){
+    @PostMapping("/createBlacklist")
+    public ResponseEntity<ResponseDto> createBlackListCard(@Valid @RequestBody BlackListDto blackListDto){
         blackListService.saveBlackListRepository(blackListDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -59,8 +65,10 @@ public class BlackListRestController {
      * @param creditCardNumber black list credit card number
      * @return
      */
-    @DeleteMapping("/blacklist/{creditCardNumber}")
-    public ResponseEntity<ResponseDto> deleteCreditCardByCreditNumber(@PathVariable String creditCardNumber) {
+    @DeleteMapping("/deleteBlacklist")
+    public ResponseEntity<ResponseDto> deleteCreditCardByCreditNumber(@RequestParam
+                                                                          @Pattern(regexp = "(^$|[0-9]{16})", message = "credit card must be 16 digits")
+                                                                          String creditCardNumber) {
         blackListService.deleteBlackListCardByCardNumber(creditCardNumber);
         return ResponseEntity
                 .status(HttpStatus.OK)
